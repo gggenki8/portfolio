3eb6334685bcebe5aeb10e2762d9f54e86d6c87c
廣城弦貴 <genki@hiroshijougentakanoMacBook-Air.local>
2025-06-11 22:55:58 +0900

    Guard bootsnap loading to development/test

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,9 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require "bundler/setup" # Set up gems listed in the Gemfile.
-#require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require "bundler/setup"  # Set up gems listed in the Gemfile
+
+# bootsnap は開発／テスト環境のみ有効化
+environment = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
+if %w[development test].include?(environment)
+  require "bootsnap/setup"  # Speed up boot time by caching expensive operations
+end
